b4538546426701160126edb42a780a26559544fa
Rupam0710 <rupampal48@gmail.com>
2024-04-22 13:33:21 +0530

    5cm - Creating the DBContext Class

---
+++ NZWalksAPI/Data/NZWalksDbContextcs.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalksAPI.Models.Domain;
+
+namespace NZWalksAPI.Data
+{
+    public class NZWalksDbContextcs : DbContext
+    {   
+        //To create a constructor - ctor
+        public NZWalksDbContextcs(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+            
+        }
+
+        public DbSet<Difficulty> Difficulties { get; set; }
+
+        public DbSet<Region> Regions { get; set; }
+
+        public DbSet<Walk> Walks { get; set; }
+    }
+}
