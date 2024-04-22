bf4c25013d036d59bc9b4d777c9802fe9d6b4c78
Rupam0710 <rupampal48@gmail.com>
2024-04-22 12:36:01 +0530

    2cm - Running & Calling ASP.NET CORE Web api

---
+++ NZWalksAPI/Controllers/StudentsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalksAPI.Controllers
+{   
+    //https:localhost:portnumber/api/controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+
+        //GET : https://localhost:portnumber/api/Students
+        [HttpGet]
+        public IActionResult GetAllStudent()
+        {
+            string[] studentname = new string[] { "Rohit", "Rupam", "Raj" };
+            return Ok(studentname);
+        }
+    }
+}
