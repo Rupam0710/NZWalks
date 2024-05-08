8e9fa9d884948d534e6528da4d444323fb568efe
Rupam0710 <rupampal48@gmail.com>
2024-05-08 16:36:08 +0530

    57cm s12 - DELETE - Deleting a Resource using web api

--- NZWalks.UI/Controllers/RegionsController.cs
+++ NZWalks.UI/Controllers/RegionsController.cs
@@ -111,5 +111,29 @@ namespace NZWalks.UI.Controllers
 
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(RegionDto request)
+        {
+            try
+            {
+                var client = httpClientFactory.CreateClient();
+
+                var httpResponseMessage = await client.DeleteAsync($"https://localhost:7211/api/regions/{request.Id}");
+
+                httpResponseMessage.EnsureSuccessStatusCode();
+
+                return RedirectToAction("Index", "Regions");
+            }
+            catch (Exception ex)
+            {
+
+                
+            }
+
+            return View("Edit");
+
+
+        }
     }
 }
